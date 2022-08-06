58ca22ffa543c10e6445e1b1a356ff18c42aa677
ArtyomBagautdinov <bagautdinovartyom@gmail.com>
2022-08-07 00:34:57 +0200

    added canvas

---
+++ src/components/Lerret.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Lerret : React.FC = () => {
+
+    return (
+        <canvas className="w-screen h-screen border border-black" ref="lerret" width="400" height="400"></canvas>
+    );
+}
+
+export default Lerret;
